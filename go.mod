2d3fd3f30d5e4bd2e7628ab96df630a30f20d318
Pavel Karagodin <pavel.karagodin@asp.com>
2022-02-19 12:02:00 +0700

    implement

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nblka/golang-united-school-homework-2
+
+go 1.17
--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +10,22 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type intCustomType int
+
+func calcEquilateralFigurineSquare(sideLen float64, sidesNum intCustomType) float64 {
+	alpha := 2 * math.Pi / float64(sidesNum)
+	return math.Pow(sideLen, 2) / (4 * math.Tan(alpha/2))
+}
+
+func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
+	switch sidesNum {
+	case 0:
+		return math.Pow(sideLen, 2) * math.Pi
+	case 3:
+		fallthrough
+	case 4:
+		return calcEquilateralFigurineSquare(sideLen, sidesNum)
+	default:
+		return 0
+	}
 }
